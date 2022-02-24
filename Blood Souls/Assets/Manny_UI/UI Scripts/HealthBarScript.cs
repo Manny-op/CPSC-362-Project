3ef7f0dcec31e12b7b20f32f3dbdf61d8971026e
emmerdroid <54683249+Manny-op@users.noreply.github.com>
2022-02-23 20:25:46 -0800

    Base UI done
    sliders for health and stamina working

--- Blood Souls/Assets/Manny_UI/UI Scripts/HealthBarScript.cs
+++ Blood Souls/Assets/Manny_UI/UI Scripts/HealthBarScript.cs
@@ -8,12 +8,18 @@ public class HealthBarScript : MonoBehaviour
     private int PlayerStam;
     //maybe have the mana count here if we do go about implementing it.
 
+    //Use the Sliders UI so create public objects
+    public Slider healthBar;
+    public Slider staminaBar;
+
+    //here would be where to use class from player.
 
     // Start is called before the first frame update
     void Start()
     {
         //for now we should go off base 100 for health stamina and such. easy to work
         //with percentages
+        //after player is done, will just pull from the player class values
         PlayerHealth = 100;
         PlayerStam = 100;
     }
@@ -21,7 +27,29 @@ public class HealthBarScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        healthBar.value = PlayerHealth;
+        staminaBar.value = PlayerStam;
+
+        //make test inputs to ensure the slider for health and stamina work
+        if(Input.GetKeyDown(KeyCode.D)) //D for damage
+        {
+            HealthChange(25); //Using 25 as an arbitray number, when properly done will use the attack damage from enemy class
+        }
+        if(Input.GetKeyDown(KeyCode.E)) //E for exhaustion
+        {
+            StaminaUse(25);  
+        }
+        if(Input.GetKeyDown (KeyCode.H)) // H for heal
+        {
+            HealthChange(-25);
+        }
+        if(Input.GetKeyDown(KeyCode.R)) // R for rest
+        {
+            StaminaUse(-25);
+        }
         
+
+
     }
 
     int HealthChange(int dam)
