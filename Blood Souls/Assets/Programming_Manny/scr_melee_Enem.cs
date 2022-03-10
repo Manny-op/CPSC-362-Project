df631b5f857779fc73d97198c58af2c75a4c5f26
emmerdroid <54683249+Manny-op@users.noreply.github.com>
2022-03-09 22:19:34 -0800

    minor script fixing

---
+++ Blood Souls/Assets/Programming_Manny/scr_melee_Enem.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_melee_Enem : MonoBehaviour
+{
+    [SerializeField]
+    private int healthPoints;
+    private int stamina;
+    private int movementSpeed;
+    private int attackDamage;
+    private int charBehavior;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
