9a4ac294bf4e0f9a6609b14678d7b02130e1e2f0
Jason <42780124+JasonT1085@users.noreply.github.com>
2022-03-17 16:59:57 -0700

    Updated parallax

--- Blood Souls/Assets/Scripts/ParallaxScrolling.cs
+++ Blood Souls/Assets/Scripts/ParallaxScrolling.cs
@@ -28,7 +28,7 @@ public class ParallaxScrolling : MonoBehaviour
 
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
 
-        if(temp > startpos + length) { startpos += length; }
-        else if (temp < startpos - length) {startpos -= length; }
+        // if(temp > startpos + length) { startpos += length; }
+        // else if (temp < startpos - length) {startpos -= length; }
     }
 }
